82da789e6994c2492fadf39299ab038a3cbf2510
zhangpanzhao <zhangpanzhao@okay.cn>
2020-03-01 22:12:18 +0800

    [test] 测试我们的ButterKnife

--- app/src/main/java/com/dapan/butterknife/demo/MainActivity.java
+++ app/src/main/java/com/dapan/butterknife/demo/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.dapan.butterknife.ButterKnife;
+import com.dapan.butterknife.UnBinder;
 import com.dapan.butterknife.annotation.ViewBind;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,9 +17,21 @@ public class MainActivity extends AppCompatActivity {
     @ViewBind(R.id.textView2)
     TextView textView2;
 
+    private UnBinder unBinder;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        unBinder = ButterKnife.bind(this);
+        textView1.setText("inject 1");
+        textView2.setText("inject 2");
+    }
+
+    @Override
+    protected void onDestroy() {
+        unBinder.unbind();
+        super.onDestroy();
     }
 }
