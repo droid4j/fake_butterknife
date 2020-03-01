80520466264d46cc430d54a8449b4c56477ecdde
zhangpanzhao <zhangpanzhao@okay.cn>
2020-03-01 20:11:57 +0800

    [test] 简单看下注解处理器的使用

--- app/src/main/java/com/dapan/butterknife/SecondActivity.java
+++ app/src/main/java/com/dapan/butterknife/SecondActivity.java
@@ -1,13 +1,13 @@
 package com.dapan.butterknife;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.dapan.butterknife.annotation.ViewBind;
 
-public class MainActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity {
 
     @ViewBind(R.id.textView1)
     TextView textView1;
